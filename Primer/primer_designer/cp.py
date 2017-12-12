1ae118dcb2af49abab4ff6cc7b3b2dd369c88106
mexak <a_urbanska@hotmail.com>
2017-12-12 20:18:28 +0100

    Primer designer - primary, datetime

--- Primer/primer_designer/cp.py
+++ Primer/primer_designer/cp.py
@@ -1,11 +1,15 @@
 
 from django.utils import timezone
-import datetime
+from datetime import datetime
 
 def my_cp(request):
 	ctx = {
-		"current_date": timezone.now(),
-#		"current_date": datetime.datetime.now(),
+#		"current_date": timezone.localtime(timezone.now()),
+		"current_date": datetime.now(),
+#		"current_date": timezone.now(),
+#		"current_date": datetime.now,
+#		"current_date": datetime.datetime.now,
+#		"current_date": timezone.now,
 		"designed_in_year" : "2017",
 	}
 	return ctx
